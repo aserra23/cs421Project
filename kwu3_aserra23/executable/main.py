d3304d6d4a4d64c22dd57a857cc648270e7dd8d6
aserra23 <aserra23@uic.edu>
2018-04-20 10:58:41 -0500

    Create main.py
    main code to run grader class

---
+++ kwu3_aserra23/executable/main.py
@@ -0,0 +1,43 @@
+from Grader import EssayGrader
+import os.path
+import os
+import nltk
+
+# this is placed here so it won't be called multiple times from the Grader module when looping
+#nltk.download()
+
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('treebank')
+
+from nltk.corpus import brown
+
+if __name__ == '__main__':
+
+    # read all the files names as string store in list
+    All_files = os.listdir("../input/testing/essays")
+
+    All_files.sort()
+
+    # open output file
+    file_writer = open("../output/result.txt", 'w')
+
+    # iterate through all essays in testing folder
+    for file in All_files:
+        filepath = "../input/testing/essays/"+file
+
+        # open file and read them as a string
+        if os.path.isfile(filepath):
+
+            essayGrader = EssayGrader(filepath, file, file_writer)
+            essayGrader.prepare_data()
+            essayGrader.determine_sub_and_final_scores()
+            essayGrader.determine_classifier()
+            essayGrader.write()
+            print(file)
+        else:
+            print("invalid filename!")
+    file_writer.close()
+    print('done')
